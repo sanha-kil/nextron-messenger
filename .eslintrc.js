713e9e17774094bda4be615b7ae8087a2eb8c293
sanha <kgpaper@naver.com>
2022-05-10 09:38:16 +0900

    init: 프로젝트 초기화

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb', // or airbnb-base
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // 설치 한경우
+    'plugin:import/errors', // 설치한 경우
+    'plugin:import/warnings', // 설치한 경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ renderer/pages/next.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+function Next() {
+  return (
+    <div>
+      <p>
+        ⚡ Electron + Next.js ⚡ -<Link href="/home">Go to home page</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Next;
